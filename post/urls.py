d796585ab20f8139e2729bf4caadf72c0dbac556
ssa <wogur981208@gmail.com>
2022-10-13 20:11:20 +0900

    Feat: 댓글 CRUD기능 추가

--- post/urls.py
+++ post/urls.py
@@ -4,9 +4,15 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
+    #post
     path('', views.index, name='index'),
     path('post/<int:post_id>/detail/', views.post_detail, name='post-detail' ),
     path('post/create/', views.post_create, name='post-create'),
     path('post/<int:post_id>/delete/', views.post_delete, name='post-delete'),
     path('post/<int:post_id>/edit/', views.post_update, name='post-update'),
+    
+    #comment
+    path('comment/<int:comment_id>/create/', views.comment_create, name='comment-create'),
+    path('comment/<int:comment_id>/delete/', views.comment_delete, name='comment-delete'),
+    path('comment/<int:comment_id>/edit/', views.comment_update, name='comment-update'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 # 리디렉션을 남발하면 서버 터진다...
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post, Comment
 from user.models import User
 
 # Create your views here.
@@ -22,7 +22,10 @@ def post_detail(request, post_id):
         context = dict()
         context['post'] = Post.objects.get(id=post_id)
         context['users'] = User.objects.all()
+        #filter와 get의 차이를 확실히 알아야한다.
+        context['comments'] = Comment.objects.filter(post_id=post_id)
         return render(request, 'post/post/post_detail.html', context=context)
+    
 
 #로그인이 한 사람만 접속이 가능하고 안되어있다면 로그인페이지로!
 @login_required(login_url='/account/signin/')
@@ -37,7 +40,7 @@ def post_create(request):
         content = request.POST.get('content')
         # 1. Post.objects.create로 만드는 법 2. Posts.save()로 만드는 법 
         #author=user도 넣어줘야 query가 비어지지 않는다. 항상 저장할 때는 생각하면서 작성
-        Posts = Post.objects.create(author=user, image=image, content=content)
+        Post.objects.create(author=user, image=image, content=content)
         return redirect('/')
     
 @login_required(login_url='/account/signin/')
@@ -63,6 +66,45 @@ def post_update(request, post_id):
         edit_post.image = request.FILES['image']
         edit_post.content = request.POST.get('content')
         edit_post.save()
+        
         return redirect('/')
     
+@login_required(login_url='/account/signin/')
+def comment_create(request, comment_id):
+    if request.method == 'POST':
         
+        user = request.user
+        post = Post.objects.get(id=comment_id)
+        content = request.POST.get('content')
+        #이렇게 하는 방식도 있지만 모델을 불러와서 comment.content = content 로 하는 방법으로 save하는 방식
+        Comment.objects.create(content=content, author=user, post=post)
+        #post_id와 post.id와 다른 것이다!!!!!!!!!!!!!!!!!
+        return redirect('post:post-detail', post.id)
+    
+@login_required(login_url='/account/signin/')
+def comment_delete(request, comment_id):
+    if request.method == 'GET':
+        
+        return render(request, 'post/post/comment_confirm_delete.html')
+    
+    elif request.method == 'POST':
+        
+        comment = Comment.objects.get(id=comment_id)
+        comment.delete() 
+        #디테일 페이지로 이동하는 방법
+        return redirect('/')
+        
+@login_required(login_url='/account/signin/')
+def comment_update(request, comment_id):
+    if request.method == 'GET':
+        context = dict()
+        context['comment'] = Comment.objects.get(id=comment_id)
+        return render(request, 'post/post/comment_update_form.html', context=context)
+    
+    elif request.method =='POST':
+        edit_comment = Comment.objects.get(id=comment_id)
+        edit_comment.content = request.POST.get('content')
+        edit_comment.save()
+        #디테일 페이지로 이동하는 방법
+        return redirect('/')
+
